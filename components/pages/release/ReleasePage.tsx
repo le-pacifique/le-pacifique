d8b235987a27cfc7deae9e614b117e4f13c0f8bf
Aymen <aymen@qq.com>
2024-11-08 23:20:20 +0800

    adds rotating 3D front + back cover release to individual release page

--- components/pages/release/ReleasePage.tsx
+++ components/pages/release/ReleasePage.tsx
@@ -10,31 +10,13 @@ export interface ReleasePageProps {
 const ReleasePage = ({ data }: ReleasePageProps) => {
   const release = data
   console.log(release, 'release')
+  console.log(release.description[0].children, 'release description')
 
   return (
     <div className="">
-      <div className="absolute inset-0 bg-artistpage-pattern bg-cover bg-center -z-0 blur-sm"></div>
-      {/* <div className="px-4 py-20 bg-[#B798DF]">
-        <h2 className="text-2xl font-bold mb-2">Collection Details</h2>
-        <p>
-          <strong>PCFQ_UNREF2:</strong>
-        </p>
-        <p>
-          <strong>Title:</strong> {collection.title}
-        </p>
-        <p>
-          <strong>Artist:</strong> {collection.artist}
-        </p>
-        <p>
-          <strong>Release Date:</strong> {collection.releaseDate}
-        </p>
-        <p>
-          <strong>Genres:</strong> {collection.genres.join(', ')}
-        </p>
-      </div> */}
-
-      <div className="mx-auto max-w-7xl px-4 lg:px-8 relative z-10 py-16 lg:py-24 tracking-tight flex items-center justify-center">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-6 lg:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+      {/* <div className="absolute inset-0 bg-artistpage-pattern bg-cover bg-center -z-0 blur-sm"></div> */}
+      <div className="mx-auto max-w-full px-4 lg:px-8 relative z-10 py-16 lg:py-24 tracking-tight flex items-center justify-center">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 lg:gap-x-56 gap-y-6 lg:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4 order-2 lg:order-1">
             <div className="lg:max-w-lg">
               <div className="flex flex-col space-y-0">
@@ -53,7 +35,7 @@ const ReleasePage = ({ data }: ReleasePageProps) => {
                 </p>
               </div>
 
-              <div className="mt-6 text-lg leading-snug">
+              <div className="mt-6 text-md leading-snug">
                 {release.description?.map((block: any) => {
                   if (block._type === 'block') {
                     return (
@@ -83,24 +65,49 @@ const ReleasePage = ({ data }: ReleasePageProps) => {
                 ))}
               </ol>
             </div>
-            {/* <div className="mt-4 flex flex-col">
-            {collection.links.map((link, index) => (
-              <a
-                key={index}
-                href={link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 hover:underline"
-              >
-                {link}
-              </a>
-            ))}
-          </div> */}
           </div>
+
+          <div className="group h-[55vh] aspect-square [perspective:1000px] order-1 lg:order-2">
+            <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+              {/* Front Face */}
+              <div className="absolute inset-0 h-full w-full rounded-xl [backface-visibility:hidden]">
+                {release?.image && (
+                  <Image
+                    className="object-cover cursor-pointer object-left h-full w-full rounded-xl"
+                    src={release.image}
+                    alt={release.title}
+                    width={2432}
+                    height={1442}
+                  />
+                )}
+              </div>
+              {/* Back Face */}
+              <div className="absolute inset-0 h-full w-full rounded-xl [backface-visibility:hidden] [transform:rotateY(180deg)]">
+                {release?.backCover ? (
+                  <Image
+                    className="object-cover cursor-pointer object-left h-full w-full rounded-xl"
+                    src={release.backCover}
+                    alt={release.title}
+                    width={2432}
+                    height={1442}
+                  />
+                ) : (
+                  <Image
+                    className="object-cover cursor-pointer object-left h-full w-full rounded-xl"
+                    src={release.image}
+                    alt={release.title}
+                    width={2432}
+                    height={1442}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+
           {/* {release?.image && (
             <Image
               alt="Product screenshot"
-              src={urlForImage(collection?.image).url()}
+              src={release?.image}
               width={2432}
               height={1442}
               className="order-1 lg:order-2 w-full max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 md:-ml-4 lg:-ml-0"
