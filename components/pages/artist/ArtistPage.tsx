39eb2fd62710d261e9b663d2fa72f569f533bce0
Aymen <aymen@qq.com>
2024-11-10 23:18:19 +0800

    minor style edits for artist page

--- components/pages/artist/ArtistPage.tsx
+++ components/pages/artist/ArtistPage.tsx
@@ -15,22 +15,22 @@ const ArtistPage = ({ data }: ArtistPageProps) => {
 
   return (
     <div
-      className="min-h-screen w-full relative"
+      className="min-h-screen w-full relative flex items-center justify-center"
       style={{ backgroundColor: backgroundColor?.hex }}
     >
       <CollectionTitle name={artist.name} />
       {noteDrawing?.image && (
         <div
-          className="absolute inset-0 bg-cover bg-center -z-10"
+          className="absolute inset-0 bg-cover bg-center"
           style={{ backgroundImage: `url(${noteDrawing.image})` }}
         ></div>
       )}
 
       <div className="mx-auto max-w-7xl px-4 lg:px-8 z-10 py-12 lg:py-24 tracking-tight">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-6 lg:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-0 lg:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4 order-2 lg:order-1">
             <div className="lg:max-w-lg">
-              <p className="mt-6 text-md tracking-tighter leading-snug">
+              <p className="mt-6 text-sm md:text-base tracking-tighter leading-snug">
                 {artist.biography}
               </p>
             </div>
@@ -41,7 +41,7 @@ const ArtistPage = ({ data }: ArtistPageProps) => {
                   href={link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-blue-500 hover:underline"
+                  className="text-blue-500 hover:underline text-sm md:text-base"
                 >
                   {link}
                 </a>
@@ -50,11 +50,11 @@ const ArtistPage = ({ data }: ArtistPageProps) => {
           </div>
           {artist?.image && (
             <Image
-              alt="Product screenshot"
+              alt="Artist image"
               src={urlForImage(artist?.image).url()}
               width={2432}
               height={1442}
-              className="order-1 lg:order-2 w-full max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 md:-ml-4 lg:-ml-0"
+              className="order-1 lg:order-2 w-full max-w-none ring-1 ring-gray-400/10 md:-ml-4 lg:-ml-0"
             />
           )}
         </div>
